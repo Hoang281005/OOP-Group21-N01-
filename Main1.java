4433db2dac1752fab76c6dc2f75309115eaaf9bb
Hoang281005 <23010143@st.phenikaa-uni.edu.vn>
2024-09-10 21:22:58 +0700

    KTGK-B1

---
+++ Main1.java
@@ -0,0 +1,27 @@
+interface Signal {
+    void transmit(); 
+}
+
+class DiscreteSignal implements Signal {
+    @Override
+    public void transmit() {
+        System.out.println("Transmitting discrete signal.");
+    }
+}
+
+class ContinuousSignal implements Signal {
+    @Override
+    public void transmit() {
+        System.out.println("Transmitting continuous signal.");
+    }
+}
+
+public class Main1 {
+    public static void main (String[] args) {
+        Signal discrete = new DiscreteSignal();
+        Signal continuous = new ContinuousSignal();
+
+        discrete.transmit();
+        continuous.transmit();
+    }
+}
